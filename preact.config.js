f38dede23b2a377ed6fa25e6864db42316862aa1
hpneo <hpneo@hotmail.com>
2017-11-18 18:22:51 -0500

    Add webpack config (alias for preact-compat)

---
+++ preact.config.js
@@ -0,0 +1,14 @@
+/**
+ * Function that mutates original webpack config.
+ * Supports asynchronous changes when promise is returned.
+ *
+ * @param {object} config - original webpack config.
+ * @param {object} env - options passed to CLI.
+ * @param {WebpackConfigHelpers} helpers - object with useful helpers when working with config.
+ **/
+export default function (config, env, helpers) {
+  const alias =  config.resolve.alias;
+
+  alias['react'] = 'preact-compat';
+  alias['react-dom'] = 'preact-compat';
+}
